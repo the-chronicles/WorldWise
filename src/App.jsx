1cc1eb58382f76117eb7752645b13a0fa17741eb
the-chronicles <oluseungideon@gmail.com>
2023-11-01 20:07:04 +0100

    Modified the global css

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import './index.css'
 
 function App() {
   return (
